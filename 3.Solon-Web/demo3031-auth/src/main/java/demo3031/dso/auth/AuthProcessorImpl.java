46d19f0edde2d7f49bf96b1d27e98348c283de8c
noear <noear@live.cn>
2022-01-06 17:53:53 +0800

    init

--- 3.Solon-Web/demo3031-auth/src/main/java/demo3031/dso/auth/AuthProcessorImpl.java
+++ 3.Solon-Web/demo3031-auth/src/main/java/demo3031/dso/auth/AuthProcessorImpl.java
@@ -4,6 +4,8 @@ import demo3031.dso.Session;
 import org.noear.grit.solon.GritAuthProcessor2;
 
 /**
+ * 如果不借用第三方的框架适配，则需要自己实现：AuthProcessor 接口
+ *
  * @author noear 2021/5/28 created
  * @see demo3031.Config
  */
