2c00df7c0f2eaf4386768fc2a13d22f32015583b
noear <noear@live.cn>
2022-01-06 18:04:52 +0800

    init

--- 3.Solon-Web/demo3013-web_jsp/src/main/java/demo3013/WebApp.java
+++ 3.Solon-Web/demo3013-web_jsp/src/main/java/demo3013/WebApp.java
@@ -5,13 +5,13 @@ import org.noear.solon.Solon;
 /**
  *
  * //资源路径说明（不用配置）
- * resources/application.properties（或 application.yml） 为应用配置文件
+ * resources/app.properties（或 app.yml） 为应用配置文件
  * resources/static/ 为静态文件根目标
  * resources/WEB-INF/view/ 为视图文件根目标（支持多视图共存）
  *
  * */
-public class MvcJspApp {
+public class WebApp {
     public static void main(String[] args) {
-        Solon.start(MvcJspApp.class, args);
+        Solon.start(WebApp.class, args);
     }
 }
