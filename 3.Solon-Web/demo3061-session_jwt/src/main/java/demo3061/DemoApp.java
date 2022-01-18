ab2001ab9f2cf2aa29d35de7a31c46abfb2ec601
noear <noear@live.cn>
2022-01-18 14:56:46 +0800

    1.6.15

---
+++ 3.Solon-Web/demo3061-session_jwt/src/main/java/demo3061/DemoApp.java
@@ -0,0 +1,7 @@
+package demo3061;
+
+/**
+ * @author noear 2022/1/18 created
+ */
+public class DemoApp {
+}
---
+++ 3.Solon-Web/demo3062-session_redis/src/main/java/demo3062/DemoApp.java
@@ -0,0 +1,7 @@
+package demo3062;
+
+/**
+ * @author noear 2022/1/18 created
+ */
+public class DemoApp {
+}
