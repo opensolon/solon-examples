afb7e31d2017ee05fbd55a04389be3197b81e7ed
noear <noear@live.cn>
2022-02-25 16:39:42 +0800

    1.6.28

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/model/AppxModel.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/model/AppxModel.java
@@ -2,9 +2,7 @@ package demo4031.model;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Data;
 
-@Data
 @TableName("appx")
 public class AppxModel {
     private Integer agroupId;
@@ -13,4 +11,24 @@ public class AppxModel {
     @TableId("app_id")
     private Integer appId;
     private Integer arIsExamine;
+
+    public Integer getAgroupId() {
+        return agroupId;
+    }
+
+    public Integer getAppId() {
+        return appId;
+    }
+
+    public Integer getArIsExamine() {
+        return arIsExamine;
+    }
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public String getNote() {
+        return note;
+    }
 }
