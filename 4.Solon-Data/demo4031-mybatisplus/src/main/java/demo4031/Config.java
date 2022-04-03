774339b74fd6d65741002c0fd476a664927d0807
noear <noear@live.cn>
2022-04-03 13:32:03 +0800

    1.6.37-m1

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/Config.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/Config.java
@@ -1,15 +1,14 @@
 package demo4031;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.solon.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.solon.plugins.inner.PaginationInnerInterceptor;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.ext.solon.Db;
-import org.apache.ibatis.plugin.Interceptor;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
-import org.noear.solon.extend.mybatisplus.integration.MybatisAdapterPlus;
 
 import javax.sql.DataSource;
 
@@ -21,25 +20,15 @@ public class Config {
     }
 
     @Bean
-    public Interceptor mybatisPlusInterceptor() {
+    public void db1_interceptor(@Db("db1") org.apache.ibatis.session.Configuration cfg) {
         MybatisPlusInterceptor plusInterceptor = new MybatisPlusInterceptor();
         plusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
 
-        return plusInterceptor;
+        cfg.addInterceptor(plusInterceptor);
     }
 
-//    @Bean
-//    public void db1_interceptor(@Db("db1") org.apache.ibatis.session.Configuration cfg) {
-//        MybatisPlusInterceptor plusInterceptor = new MybatisPlusInterceptor();
-//        plusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-//
-//        cfg.addInterceptor(plusInterceptor);
-//    }
-//
-//    @Bean
-//    public void db1_globalConfig(@Db("db1") MybatisAdapterPlus adapterPlus){
-//        adapterPlus.getGlobalConfig()
-//                .getDbConfig()
-//                .setLogicDeleteField("delete");
-//    }
+    @Bean
+    public void db1_globalConfig(@Db("db1") GlobalConfig globalConfig) {
+        globalConfig.setBanner(false);
+    }
 }
