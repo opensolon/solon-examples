432fd38fd5b5147d82a7fec89f5eedf7b33c5f15
noear <noear@live.cn>
2022-05-06 22:15:58 +0800

    1.7.5

---
+++ 5.Solon-Job/demo5091-job_water/src/main/java/demo5091/DemoJobApp.java
@@ -0,0 +1,12 @@
+package demo5091;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/5/25 created
+ */
+public class DemoJobApp {
+    public static void main(String[] args) {
+        Solon.start(DemoJobApp.class, args);
+    }
+}
