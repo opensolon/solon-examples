7c2673ea070095b8be5a80cc0c52a7b2fe94e5c1
noear <noear@live.cn>
2022-05-08 00:36:43 +0800

    1.7.6

--- 9.Solon-Cloud/demo9071-opentracing_jeager/src/main/java/demo9071/client/dso/service/OrderService.java
+++ 9.Solon-Cloud/demo9071-opentracing_jeager/src/main/java/demo9071/client/dso/service/OrderService.java
@@ -1,6 +1,6 @@
 package demo9071.client.dso.service;
 
-import org.noear.solon.cloud.opentracing.TracingUtil;
+import org.noear.solon.cloud.opentracing.Spans;
 import org.noear.solon.cloud.opentracing.annotation.Tracing;
 import org.noear.solon.extend.aspect.annotation.Service;
 
@@ -11,7 +11,7 @@ import org.noear.solon.extend.aspect.annotation.Service;
 public class OrderService {
     @Tracing(tags = "订单=${orderId}")
     public void orderCreate(String orderId) {
-        TracingUtil.activeSpan(span -> span.setTag("用户", 1));
+        Spans.active(span -> span.setTag("用户", 1));
 
         System.out.println("print - test");
     }
