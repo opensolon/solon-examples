3d040f9f6d693170c4ed6c7f1ab50fea9151b4ea
noear <noear@live.cn>
2022-08-14 11:19:27 +0800

    1.10.0

---
+++ 1.Solon/demo1011-hotplug_plugin1/src/test/java/demo1011/debug/App.java
@@ -0,0 +1,14 @@
+package demo1011.debug;
+
+import org.noear.solon.Solon;
+
+/**
+ * 用于插件自动行调试用
+ *
+ * @author noear 2022/8/14 created
+ */
+public class App {
+    public static void main(String[] args){
+        Solon.start(App.class, args);
+    }
+}
