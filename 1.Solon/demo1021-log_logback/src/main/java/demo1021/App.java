d982d6e567eecbdd31cd3b832e9cc69ad6071e7b
noear <noear@live.cn>
2022-09-10 16:51:34 +0800

    1.10.2

---
+++ 1.Solon/demo1021-log_logback/src/main/java/demo1021/App.java
@@ -0,0 +1,26 @@
+package demo1021;
+
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.schedule.annotation.EnableScheduling;
+import org.noear.solon.schedule.annotation.Scheduled;
+
+import java.util.Date;
+
+/**
+ * @author noear 2022/9/10 created
+ */
+@Slf4j
+@EnableScheduling
+@Component
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args);
+    }
+
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void test() {
+        log.info("{}", new Date());
+    }
+}
