45a90c66ba2c1dd56318912725b056f55c639384
noear <noear@live.cn>
2022-09-22 08:12:10 +0800

    1.10.3

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/Config.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/Config.java
@@ -1,6 +1,8 @@
 package demo4031;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.solon.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.solon.plugins.inner.PaginationInnerInterceptor;
 import com.zaxxer.hikari.HikariDataSource;
@@ -19,11 +21,14 @@ public class Config {
     }
 
     @Bean
-    public void db1_cfg(@Db("db1") org.apache.ibatis.session.Configuration cfg) {
+    public void db1_cfg(@Db("db1") MybatisConfiguration cfg,
+                        @Db("db1") GlobalConfig globalConfig) {
         MybatisPlusInterceptor plusInterceptor = new MybatisPlusInterceptor();
         plusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
 
         cfg.setCacheEnabled(false);
         cfg.addInterceptor(plusInterceptor);
+
+        //globalConfig.setIdentifierGenerator(null);
     }
 }
