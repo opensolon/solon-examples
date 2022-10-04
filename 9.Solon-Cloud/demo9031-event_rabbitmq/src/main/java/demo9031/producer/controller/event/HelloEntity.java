c4a3199cc9cbd10d8fb6c294e72d4e0f3f9867ea
noear <noear@live.cn>
2022-10-04 23:25:25 +0800

    1.10.5

--- 9.Solon-Cloud/demo9031-event_rabbitmq/src/main/java/demo9031/producer/controller/event/HelloEntity.java
+++ 9.Solon-Cloud/demo9031-event_rabbitmq/src/main/java/demo9031/producer/controller/event/HelloEntity.java
@@ -1,7 +1,7 @@
 package demo9031.producer.controller.event;
 
 import org.noear.solon.cloud.annotation.CloudEvent;
-import org.noear.solon.cloud.extend.cloudeventplus.CloudEventEntity;
+import org.noear.solon.cloud.eventplus.CloudEventEntity;
 
 /**
  * @author noear 2021/11/5 created
