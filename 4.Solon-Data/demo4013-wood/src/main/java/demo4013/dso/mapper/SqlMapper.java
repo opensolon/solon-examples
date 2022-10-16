20dba7172a34d98bf402fb25a96b8629c997ed5c
noear <noear@live.cn>
2022-10-16 17:57:09 +0800

    1.10.7

---
+++ 4.Solon-Data/demo4013-wood/src/main/java/demo4013/dso/mapper/SqlMapper.java
@@ -0,0 +1,27 @@
+package demo4013.dso.mapper;
+
+import demo4013.model.AppxModel;
+import org.noear.wood.xml.Namespace;
+
+import java.util.List;
+import java.util.Set;
+
+@Namespace("demo4013.dso.mapper.SqlMapper")
+public interface SqlMapper{
+    //随便取条数据的ID
+    int appx_get() ;
+
+    //随便取条数据的ID
+    Integer appx_get0() ;
+
+    //根据id取条数据
+    AppxModel appx_get2(int app_id) ;
+
+    List<AppxModel> appx_getlist(int app_id) ;
+
+    List<Integer> appx_getids() ;
+
+    Set<Integer> appx_getids2() ;
+
+    void appx_add() ;
+}
---
+++ 4.Solon-Data/demo4013-wood/src/main/java/demo4013/dso/service/AppService.java
@@ -0,0 +1,18 @@
+package demo4013.dso.service;
+
+/**
+ * @author noear 2021/11/17 created
+ */
+public interface AppService {
+    Object getApp(int app_id) throws Exception;
+    Object getApp(String app_id) throws Exception;
+    void addApp();
+    void addApp2();
+    void addApp2_2();
+    void addApp3();
+    boolean addApp4();
+    boolean addApp52();
+    boolean addApp75();
+    void addApp5();
+    void addApp6();
+}
