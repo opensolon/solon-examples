f16069cd761a403fb3ce21ce4272e5fb16cde0a7
noear <noear@live.cn>
2022-10-30 22:31:30 +0800

    1.10.10

---
+++ 1.Solon/demo1011-hotplug_plugin1/src/main/java/demo1011/plugin1/dso/service/WeChatMpServiceImpl.java
@@ -0,0 +1,11 @@
+package demo1011.plugin1.dso.service;
+
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.noear.solon.aspect.annotation.Service;
+
+/**
+ * @author noear 2022/10/29 created
+ */
+@Service
+public class WeChatMpServiceImpl extends WxMpServiceImpl {
+}
