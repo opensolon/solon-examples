bcdf120936296f71ce3d823fd7bdf19ffe5b56c9
noear <noear@live.cn>
2022-10-31 12:58:28 +0800

    1.10.10

---
+++ 7.Solon-Remoting-Rpc/demo7022-forest/src/test/java/gitee/Demo.java
@@ -0,0 +1,19 @@
+package gitee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.noear.solon.annotation.Inject;
+import org.noear.solon.test.SolonJUnit4ClassRunner;
+
+@RunWith(SolonJUnit4ClassRunner.class)
+public class Demo {
+    @Inject
+    TestGiteeApi api;
+
+    @Test
+    public void test() {
+        String ret = api.search("solon");
+        System.out.println(ret);
+        assert ret.contains("html");
+    }
+}
---
+++ 7.Solon-Remoting-Rpc/demo7022-forest/src/test/java/gitee/TestGiteeApi.java
@@ -0,0 +1,15 @@
+package gitee;
+
+import com.dtflys.forest.annotation.*;
+
+/**
+ * @BaseRequest（baseURL ="https://www.gitee.com"）  直接使用值
+ * @BaseRequest（baseURL ="#{test.gitee}"）  使用配置文件中的参数
+ * @BaseRequest(baseURL = "upstream://test") 使用配置的upstream
+ */
+@ForestClient
+@BaseRequest(baseURL = "upstream://gitee")
+public interface TestGiteeApi {
+    @Get
+    String search(@Query("q") String q);
+}
