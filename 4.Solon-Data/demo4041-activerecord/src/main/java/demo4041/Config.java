0fdacf6d06a3825c8e76b5dd1b2139b6b72e0e54
noear <noear@live.cn>
2022-11-05 17:21:38 +0800

    1.10.11

--- 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/Config.java
+++ 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/Config.java
@@ -1,9 +1,12 @@
 package demo4041;
 
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.zaxxer.hikari.HikariDataSource;
+import org.noear.solon.Solon;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.extend.activerecord.annotation.Db;
 
 import javax.sql.DataSource;
 
@@ -20,5 +23,21 @@ public class Config {
     @Bean("db2")
     public DataSource db2(@Inject("${test.db2}") HikariDataSource dataSource) {
         return dataSource;
+    }
+
+    @Bean
+    public void db1Init(@Db("db1") ActiveRecordPlugin arp){
+        //启用开发或调试模式（可以打印sql）
+        if (Solon.cfg().isDebugMode() || Solon.cfg().isFilesMode()) {
+            arp.setDevMode(true);
+        }
+    }
+
+    @Bean
+    public void db2Init(@Db("db2") ActiveRecordPlugin arp){
+        //启用开发或调试模式（可以打印sql）
+        if (Solon.cfg().isDebugMode() || Solon.cfg().isFilesMode()) {
+            arp.setDevMode(true);
+        }
     }
 }
--- 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/DemoApp.java
+++ 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/DemoApp.java
@@ -1,25 +1,12 @@
 package demo4041;
 
-import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import org.noear.snack.ONode;
-import org.noear.snack.core.Feature;
 import org.noear.solon.Solon;
-import org.noear.solon.core.event.PluginLoadEndEvent;
-import org.noear.solon.core.handle.RenderManager;
 
 /**
  * @author noear 2021/5/24 created
  */
 public class DemoApp {
     public static void main(String[] args) {
-        Solon.start(DemoApp.class, args, app -> {
-            //订阅事件，后以定制
-            app.onEvent(ActiveRecordPlugin.class, arp -> {
-                //启用开发或调试模式（可以打印sql）
-                if (Solon.cfg().isDebugMode() || Solon.cfg().isFilesMode()) {
-                    arp.setDevMode(true);
-                }
-            });
-        });
+        Solon.start(DemoApp.class, args);
     }
 }
