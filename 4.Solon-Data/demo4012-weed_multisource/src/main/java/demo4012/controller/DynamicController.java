6889adc0bcff39eb43eabdd3e02500cf1cc564ef
noear <noear@live.cn>
2022-11-23 21:48:21 +0800

    1.11.1

---
+++ 4.Solon-Data/demo4012-weed_multisource/src/main/java/demo4012/controller/DynamicController.java
@@ -0,0 +1,50 @@
+package demo4012.controller;
+
+import demo4012.dso.mapper.SqlMapper;
+import demo4012.model.AppxModel;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.data.datasource.DynamicDsUtils;
+import org.noear.solon.data.datasource.annotation.DynamicDs;
+import org.noear.weed.annotation.Db;
+
+/**
+ * @author noear 2022/11/23 created
+ */
+@Mapping("/dynamic/")
+@Controller
+public class DynamicController {
+    /**
+     * 使用SqlMapper默认的数据库注入
+     * */
+    @Db("db2")
+    SqlMapper sqlMapper1;
+
+
+
+    @DynamicDs("db_rock1")
+    @Mapping("/test1")
+    public AppxModel test1() throws Exception{
+        System.out.println("ds===" + DynamicDsUtils.getCurrent());
+
+        return sqlMapper1.appx_get2(1);
+    }
+
+
+    @DynamicDs("db_rock2")
+    @Mapping("/test2")
+    public AppxModel test2() throws Exception{
+        System.out.println("ds===" + DynamicDsUtils.getCurrent());
+
+        return sqlMapper1.appx_get2(2);
+    }
+
+
+    @DynamicDs
+    @Mapping("/test3")
+    public AppxModel test3() throws Exception{
+        System.out.println("ds===" + DynamicDsUtils.getCurrent());
+
+        return sqlMapper1.appx_get2(3);
+    }
+}
---
+++ 4.Solon-Data/demo4012-weed_multisource/src/test/java/features/DynamicTest.java
@@ -0,0 +1,34 @@
+package features;
+
+import demo4012.DemoApp;
+import demo4012.model.AppxModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.noear.snack.ONode;
+import org.noear.solon.test.HttpTestBase;
+import org.noear.solon.test.SolonJUnit4ClassRunner;
+import org.noear.solon.test.SolonTest;
+import org.noear.weed.DbContext;
+import org.noear.weed.annotation.Db;
+
+@RunWith(SolonJUnit4ClassRunner.class)
+@SolonTest(DemoApp.class)
+public class DynamicTest extends HttpTestBase {
+
+
+    @Test
+    public void test() throws Exception {
+        String json1 = path("/dynamic/test1").get();
+        String json2 = path("/dynamic/test2").get();
+        String json3 = path("/dynamic/test3").get();
+
+        AppxModel app1 = ONode.deserialize(json1, AppxModel.class);
+        assert app1.getApp_id() == 1;
+
+        AppxModel app2 = ONode.deserialize(json2, AppxModel.class);
+        assert app2.getApp_id() == 2;
+
+        AppxModel app3 = ONode.deserialize(json3, AppxModel.class);
+        assert app3.getApp_id() == 3;
+    }
+}
