6c80b71fe9584162a6d92d98c4a63d713556694f
noear <noear@live.cn>
2022-12-01 14:47:05 +0800

    1.11.3

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/UserMapper.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package demo4031.dso.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import demo4031.model.UserModel;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author noear 2022/4/5 created
  */
+@Mapper
 public interface UserMapper extends BaseMapper<UserModel> {
 }
