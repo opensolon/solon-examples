4cb86261ab64de4cb1c2c8816aee89e1737ea52b
noear <noear@live.cn>
2022-12-12 23:05:11 +0800

    1.11.5

---
+++ 9.Solon-Cloud/demo9037-event_aliyun_ons/src/main/java/demo9037/producer/controller/event/EVENT_hello_demo.java
@@ -0,0 +1,23 @@
+package demo9037.producer.controller.event;
+
+import org.noear.snack.ONode;
+import org.noear.solon.cloud.CloudEventHandler;
+import org.noear.solon.cloud.annotation.CloudEvent;
+import org.noear.solon.cloud.model.Event;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author noear 2021/1/27 created
+ */
+@CloudEvent("hello.demo")
+public class EVENT_hello_demo implements CloudEventHandler {
+    @Override
+    public boolean handle(Event event) throws Throwable {
+        //
+        //学示处理成功效果
+        //
+        System.out.println("aaa: " + LocalDateTime.now() + ONode.stringify(event));
+        return true;
+    }
+}
