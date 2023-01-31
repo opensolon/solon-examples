5c7f847cd0dcdcf7f6d8eee8077a5c08771500c4
noear <noear@live.cn>
2023-01-31 08:28:21 +0800

    2.0.0

--- 9.Solon-Cloud/demo9064-job_powerjob/src/main/java/demo9064/controller/Job2Com.java
+++ 9.Solon-Cloud/demo9064-job_powerjob/src/main/java/demo9064/controller/Job2Com.java
@@ -3,6 +3,7 @@ package demo9064.controller;
 import org.noear.snack.ONode;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.cloud.annotation.CloudJob;
+import tech.powerjob.worker.core.processor.ProcessResult;
 import tech.powerjob.worker.core.processor.TaskContext;
 
 /**
@@ -14,8 +15,10 @@ public class Job2Com {
      * 会转为 PowerJobProxy implements BasicProcessor 处理
      * */
     @CloudJob("job2")
-    public void job2(TaskContext taskContext){
+    public ProcessResult job2(TaskContext taskContext){
         System.out.println(ONode.stringify(taskContext));
         System.out.println("xxxxx - job2");
+
+        return new ProcessResult(true,"test!");
     }
 }
