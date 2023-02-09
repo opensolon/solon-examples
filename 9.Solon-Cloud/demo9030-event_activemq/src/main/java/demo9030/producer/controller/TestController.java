9600ebac1c4a06e7e6fe17811e34557cbbe2b710
noear <noear@live.cn>
2023-02-09 19:49:47 +0800

    2.1.0

---
+++ 9.Solon-Cloud/demo9030-event_activemq/src/main/java/demo9030/producer/controller/TestController.java
@@ -0,0 +1,50 @@
+package demo9030.producer.controller;
+
+import demo9030.producer.controller.event.HelloEntity;
+import org.noear.solon.Utils;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.cloud.model.Event;
+
+import java.util.Date;
+
+/**
+ * @author noear 2021/1/27 created
+ */
+@Controller
+public class TestController {
+    @Mapping("/test")
+    public Object test(String msg) {
+        if (Utils.isEmpty(msg)) {
+            msg = "demo2";
+        }
+
+        Event event = new Event("hello.demo", msg);
+        return CloudClient.event().publish(event);
+    }
+
+    @Mapping("/test2")
+    public Object test2(String msg) {
+        if (Utils.isEmpty(msg)) {
+            msg = "demo2";
+        }
+
+        long time = System.currentTimeMillis() + 10 * 1000;
+
+        Event event = new Event("hello.demo2", msg).group("test").scheduled(new Date(time));
+        return CloudClient.event().publish(event);
+    }
+
+
+    @Mapping("/test3")
+    public Object test3(String msg) {
+        if (Utils.isEmpty(msg)) {
+            msg = "noear";
+        }
+
+        HelloEntity entity = new HelloEntity();
+        entity.name = "noear";
+        return entity.publish();
+    }
+}
---
+++ 9.Solon-Cloud/demo9030-event_activemq/src/test/java/consumer/event/HelloEntitySubscribe.java
@@ -0,0 +1,16 @@
+package consumer.event;
+
+import org.noear.solon.annotation.Component;
+import org.noear.solon.cloud.eventplus.CloudEventSubscribe;
+
+/**
+ * @author noear 2021/11/5 created
+ */
+@Component
+public class HelloEntitySubscribe {
+    @CloudEventSubscribe
+    public boolean hello(HelloEntity event){
+        System.out.println("HelloEntitySubscribe:: " + event.name);
+        return true;
+    }
+}
