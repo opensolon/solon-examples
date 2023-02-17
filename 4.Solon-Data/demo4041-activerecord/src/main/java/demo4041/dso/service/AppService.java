3ee0fca8be777979bbb67674f296007892d15954
noear <noear@live.cn>
2023-02-17 10:59:23 +0800

    Update AppService.java

--- 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/dso/service/AppService.java
+++ 4.Solon-Data/demo4041-activerecord/src/main/java/demo4041/dso/service/AppService.java
@@ -1,26 +1,26 @@
 package demo4041.dso.service;
 
-import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.DbPro;
 import org.noear.solon.data.annotation.Cache;
 import org.noear.solon.data.annotation.Tran;
 import org.noear.solon.data.tran.TranPolicy;
 import org.noear.solon.aspect.annotation.Service;
+import org.noear.solon.extend.activerecord.annotation.Db;
 
 
 @Service
 public class AppService {
     //测试注入
-    @org.noear.solon.extend.activerecord.annotation.Db
+    @Db
     DbPro db1;
 
     public Object getApp(int app_id) throws Exception {
-        return Db.template("appx_get2", app_id).findFirst();
+        return db1.template("appx_get2", app_id).findFirst();
         //return sqlMapper1.appx_get2(app_id);
     }
 
     public Object getApp(String app_id) throws Exception {
-        return Db.template("appx_get2", Integer.parseInt(app_id)).findFirst();
+        return db1.template("appx_get2", Integer.parseInt(app_id)).findFirst();
         //return sqlMapper1.appx_get2(Integer.parseInt(app_id));
     }
 
