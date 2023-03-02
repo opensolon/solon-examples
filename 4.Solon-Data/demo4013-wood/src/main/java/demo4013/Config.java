e2a5fb38bcc133c397450992ad7cb6bc7f01c885
noear <noear@live.cn>
2023-03-02 16:21:18 +0800

    2.2.0

--- 4.Solon-Data/demo4013-wood/src/main/java/demo4013/Config.java
+++ 4.Solon-Data/demo4013-wood/src/main/java/demo4013/Config.java
@@ -4,6 +4,8 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.wood.DbContext;
+import org.noear.wood.annotation.Db;
 
 import javax.sql.DataSource;
 
@@ -13,5 +15,10 @@ public class Config {
     public DataSource db1(@Inject("${test.db1}") HikariDataSource ds) throws Exception{
         return ds;
     }
+
+//    @Bean
+//    public void db2Init(@Db DbContext db) throws Exception{
+//        db.exe("insert into user(id) values(1),(2)");
+//    }
 }
 
