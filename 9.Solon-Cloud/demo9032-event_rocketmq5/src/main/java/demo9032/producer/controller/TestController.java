1893bd13255c65342e598d48b7e749196323bd5b
noear <noear@live.cn>
2023-03-09 20:45:18 +0800

    2.2.2

---
+++ 9.Solon-Cloud/demo9032-event_rocketmq5/src/main/java/demo9032/producer/controller/TestController.java
@@ -0,0 +1,41 @@
+package demo9032.producer.controller;
+
+import org.noear.solon.Utils;
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.annotation.Mapping;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.cloud.model.Event;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author noear 2021/1/27 created
+ */
+@Controller
+public class TestController {
+    @Mapping("/test")
+    public Object test(String msg) {
+        if(Utils.isEmpty(msg)){
+            msg = "demo";
+        }
+
+        Event event = new Event("hello.demo", msg);
+        return CloudClient.event().publish(event);
+    }
+
+    @Mapping("/test2")
+    public Object test2(String msg) {
+        if (Utils.isEmpty(msg)) {
+            msg = "demo2";
+        }
+
+        long time = System.currentTimeMillis() + 10 * 1000;
+
+        Event event = new Event("hello.demo2", msg).group("test").scheduled(new Date(time));
+        return CloudClient.event().publish(event);
+    }
+}
---
+++ 9.Solon-Cloud/demo9032-event_rocketmq5/src/test/java/consumer/event/EVENT_hello_demo_1.java
@@ -0,0 +1,23 @@
+package consumer.event;
+
+import org.noear.snack.ONode;
+import org.noear.solon.cloud.CloudEventHandler;
+import org.noear.solon.cloud.annotation.CloudEvent;
+import org.noear.solon.cloud.model.Event;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author noear 2021/1/27 created
+ */
+@CloudEvent("hello.demo")
+public class EVENT_hello_demo_1 implements CloudEventHandler {
+    @Override
+    public boolean handle(Event event) throws Throwable {
+        //
+        //学示处理成功效果
+        //
+        System.out.println("bbb: " + LocalDateTime.now() + ONode.stringify(event));
+        return true;
+    }
+}
