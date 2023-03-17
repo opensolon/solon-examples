25daf00aa58cdd7a4ae6166d849dc579bf87007a
noear <noear@live.cn>
2023-03-17 20:54:24 +0800

    2.2.5

--- 2.Solon_Advanced/demo2002-external_ext/src/main/java/demo2002/App.java
+++ 2.Solon_Advanced/demo2002-external_ext/src/main/java/demo2002/App.java
@@ -1,18 +1,16 @@
 package demo2002;
 
 import org.noear.solon.Solon;
-import org.noear.solon.web.staticfiles.StaticMappings;
-import org.noear.solon.web.staticfiles.repository.ExtendStaticRepository;
+import org.noear.solon.annotation.SolonMain;
 
 /**
  * @author noear 2022/1/7 created
  */
+@SolonMain
 public class App {
     public static void main(String[] args) {
 
-        Solon.start(App.class, args, app -> {
-            StaticMappings.add("/", new ExtendStaticRepository());
-        });
+        Solon.start(App.class, args);
 
         System.out.println("读取配置 demo_name：" + Solon.cfg().get("demo_name", "1"));
     }
