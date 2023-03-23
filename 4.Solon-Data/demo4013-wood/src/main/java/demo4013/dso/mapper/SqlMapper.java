8d8d20b2229fc5175ee115ff45977d1bf64d5952
noear <noear@live.cn>
2023-03-23 17:16:39 +0800

    2.2.7-SNAPSHOT

--- 4.Solon-Data/demo4013-wood/src/main/java/demo4013/dso/mapper/SqlMapper.java
+++ 4.Solon-Data/demo4013-wood/src/main/java/demo4013/dso/mapper/SqlMapper.java
@@ -1,34 +1,26 @@
 package demo4013.dso.mapper;
 
-import java.math.*;
-import java.sql.SQLException;
-import java.time.*;
 import java.util.*;
 
-import org.noear.wood.BaseMapper;
-import org.noear.wood.DataItem;
-import org.noear.wood.DataList;
-import org.noear.wood.annotation.Db;
 import org.noear.wood.xml.Namespace;
 import demo4013.model.*;
 
-@Db("db1")
 @Namespace("demo4013.dso.mapper.SqlMapper")
 public interface SqlMapper{
     //随便取条数据的ID
-    int appx_get() throws SQLException;
+    int appx_get() ;
 
     //随便取条数据的ID
-    Integer appx_get0() throws SQLException;
+    Integer appx_get0() ;
 
     //根据id取条数据
-    AppxModel appx_get2(int app_id) throws SQLException;
+    AppxModel appx_get2(int app_id) ;
 
-    List<AppxModel> appx_getlist(int app_id) throws SQLException;
+    List<AppxModel> appx_getlist(int app_id) ;
 
-    List<Integer> appx_getids() throws SQLException;
+    List<Integer> appx_getids() ;
 
-    Set<Integer> appx_getids2() throws SQLException;
+    Set<Integer> appx_getids2() ;
 
-    void appx_add() throws SQLException;
+    void appx_add() ;
 }
