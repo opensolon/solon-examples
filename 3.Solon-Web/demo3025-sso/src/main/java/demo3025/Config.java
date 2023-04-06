cb8f2e0c1958c5a2ed0984f3e5889fdf07bf97bc
noear <noear@live.cn>
2023-04-07 00:00:11 +0800

    2.2.10-SNAPSHOT

--- 3.Solon-Web/demo3025-sso/src/main/java/demo3025/Config.java
+++ 3.Solon-Web/demo3025-sso/src/main/java/demo3025/Config.java
@@ -6,8 +6,8 @@ import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.validation.annotation.LoginedChecker;
 import org.noear.solon.web.sso.SsoLoginedChecker;
-import org.noear.solon.web.sso.SsoService;
-import org.noear.solon.web.sso.SsoServiceImpl;
+import org.noear.solon.web.sso.SsoStorage;
+import org.noear.solon.web.sso.impl.SsoStorageOfRedis;
 
 /**
  * 主要是为了构建： SsoService 和 SsoLoginedChecker
@@ -17,14 +17,14 @@ import org.noear.solon.web.sso.SsoServiceImpl;
 @Configuration
 public class Config {
     @Bean
-    public SsoService ssoService(@Inject("${demo.redis}") RedisClient redisClient) {
+    public SsoStorage ssoService(@Inject("${demo.redis}") RedisClient redisClient) {
         // RedisClient 可以通过配置自己构建
         // 如果使用的是别的Redis框架，可以自己实现SsoService（只有两个接口）
-        return new SsoServiceImpl(redisClient);
+        return new SsoStorageOfRedis(redisClient);
     }
 
     @Bean
-    public LoginedChecker ssoLoginedChecker(@Inject SsoService ssoService) {
-        return new SsoLoginedChecker(ssoService);
+    public LoginedChecker ssoLoginedChecker() {
+        return new SsoLoginedChecker();
     }
 }
