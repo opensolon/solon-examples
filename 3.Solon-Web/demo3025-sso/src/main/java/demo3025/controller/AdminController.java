77375bff64742e2483b81a87829bc29fe8fd5208
noear <noear@live.cn>
2023-04-06 08:41:33 +0800

    2.2.9

--- 3.Solon-Web/demo3025-sso/src/main/java/demo3025/controller/AdminController.java
+++ 3.Solon-Web/demo3025-sso/src/main/java/demo3025/controller/AdminController.java
@@ -4,6 +4,9 @@ import org.noear.solon.annotation.Controller;
 import org.noear.solon.validation.annotation.Logined;
 
 /**
+ * 如果没登录，会出错
+ * 如果用户换电脑登录，也会出错（即，之前登录的电脑就不能用了）
+ *
  * @author noear 2023/4/5 created
  */
 @Logined //可以使用验证注解了，并且是基于sso的
