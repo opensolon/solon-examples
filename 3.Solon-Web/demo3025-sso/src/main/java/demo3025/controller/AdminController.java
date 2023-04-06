8015665fec0fdc3b074b7e98b99f41fa7cea4d7b
noear <noear@live.cn>
2023-04-06 08:39:35 +0800

    2.2.9

---
+++ 3.Solon-Web/demo3025-sso/src/main/java/demo3025/controller/AdminController.java
@@ -0,0 +1,13 @@
+package demo3025.controller;
+
+import org.noear.solon.annotation.Controller;
+import org.noear.solon.validation.annotation.Logined;
+
+/**
+ * @author noear 2023/4/5 created
+ */
+@Logined //可以使用验证注解了，并且是基于sso的
+@Controller
+public class AdminController extends BaseController{
+
+}
---
+++ 3.Solon-Web/demo3025-sso/src/main/java/demo3025/controller/BaseController.java
@@ -0,0 +1,10 @@
+package demo3025.controller;
+
+import org.noear.solon.validation.annotation.Valid;
+
+/**
+ * @author noear 2023/4/5 created
+ */
+@Valid //通过基类，为扩展类增加验证支持
+public class BaseController {
+}
