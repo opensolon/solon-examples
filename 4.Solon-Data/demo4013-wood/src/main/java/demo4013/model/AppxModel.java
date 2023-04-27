8fc4fb992aedac364317d68bb59aadc4a1564983
noear <noear@live.cn>
2023-04-27 23:22:25 +0800

    2.2.14-SNAPSHOT

--- 4.Solon-Data/demo4013-wood/src/main/java/demo4013/model/AppxModel.java
+++ 4.Solon-Data/demo4013-wood/src/main/java/demo4013/model/AppxModel.java
@@ -3,9 +3,11 @@ package demo4013.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class AppxModel {
+public class AppxModel implements Serializable {
     private int agroup_id;
     private String note;
     private String app_key;
