ffbc53b8abe233621fc9b88965d12045ec2d204d
weizhile <348358584@qq.com>
2023-05-22 17:15:41 +0800

    新增zipkin-solon-cloud-plugin测试类demo9073-zipkin

---
+++ 9.Solon-Cloud/demo9073-zipkin/src/main/java/demo9073/server/dso/service/UserService.java
@@ -0,0 +1,20 @@
+package demo9073.server.dso.service;
+
+import org.noear.solon.annotation.ProxyComponent;
+import org.noear.solon.cloud.tracing.annotation.Tracing;
+
+/**
+ * @author noear 2022/5/7 created
+ */
+@ProxyComponent
+public class UserService extends BaseService{
+    @Tracing(name = "获取用户", tags = "用户=${name}")
+    public String getUser(String name) {
+        System.out.println("获取用户...");
+        return name;
+    }
+
+    public String updateUser(String name){
+        return name;
+    }
+}
