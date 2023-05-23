99cc0b10bff5d67458ba404bd7990990da332214
noear <noear@live.cn>
2023-05-23 08:27:28 +0800

    2.3.0-SNAPSHOT

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mybatisplus_ext/MyBaseMapper.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mybatisplus_ext/MyBaseMapper.java
@@ -3,6 +3,5 @@ package demo4031.dso.mybatisplus_ext;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface MyBaseMapper<T> extends BaseMapper<T> {
 }
--- 4.Solon-Data/demo4035-mybatisflex/src/main/java/demo4035/dso/mybatisflex_ext/MyBaseMapper.java
+++ 4.Solon-Data/demo4035-mybatisflex/src/main/java/demo4035/dso/mybatisflex_ext/MyBaseMapper.java
@@ -1,8 +1,6 @@
 package demo4035.dso.mybatisflex_ext;
 
 import com.mybatisflex.core.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
 public interface MyBaseMapper<T> extends BaseMapper<T> {
 }
