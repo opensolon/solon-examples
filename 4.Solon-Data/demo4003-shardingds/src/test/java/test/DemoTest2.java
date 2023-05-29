eafbee823cddf8a10eb3d3f55584c5fed375307a
noear <noear@live.cn>
2023-05-29 17:38:30 +0800

    2.3.1-SNAPSHOT

---
+++ 4.Solon-Data/demo4003-shardingds/src/test/java/test/DemoTest2.java
@@ -0,0 +1,58 @@
+package test;
+
+import org.noear.solon.Solon;
+import org.noear.solon.test.SolonTest;
+import org.noear.solon.test.annotation.TestPropertySource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author noear 2023/5/29 created
+ */
+@TestPropertySource("classpath:app.yml")
+@SolonTest
+public class DemoTest2 {
+    public static void main(String[] args) throws Exception{
+        Solon.start(DemoTest2.class, args);
+
+        DataSource dataSource = Solon.context().getBean(DataSource.class);
+        // 读
+        Connection connectionRead = dataSource.getConnection();
+        Statement statementRead = connectionRead.createStatement();
+        ResultSet resultSet = statementRead.executeQuery("select 1");
+        System.out.println("execute = " + resultSet);
+        statementRead.close();
+        connectionRead.close();
+        // ---------------------------------------------------------------------------
+        // 写
+        Connection connectionWrite = dataSource.getConnection();
+        connectionWrite.setAutoCommit(false);
+        Statement statementWrite = connectionWrite.createStatement();
+        statementWrite.executeUpdate("INSERT INTO e_dict (create_by, create_time, update_by, update_time, code, name, remark, erupt_tenant_id)\n" +
+                "VALUES (null, null, null, null, null, '1', null, null);\n" +
+                "\n");
+        connectionWrite.rollback();
+
+        System.out.println("------------------ Sharding Ds 2 ------------------");
+        dataSource = Solon.context().getBean("ds2");
+        // 读
+        connectionRead = dataSource.getConnection();
+        statementRead = connectionRead.createStatement();
+        resultSet = statementRead.executeQuery("select 1");
+        System.out.println("execute = " + resultSet);
+        statementRead.close();
+        connectionRead.close();
+        // ---------------------------------------------------------------------------
+        // 写
+        connectionWrite = dataSource.getConnection();
+        connectionWrite.setAutoCommit(false);
+        statementWrite = connectionWrite.createStatement();
+        statementWrite.executeUpdate("INSERT INTO e_dict (create_by, create_time, update_by, update_time, code, name, remark, erupt_tenant_id)\n" +
+                "VALUES (null, null, null, null, null, '1', null, null);\n" +
+                "\n");
+        connectionWrite.rollback();
+    }
+}
