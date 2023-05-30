565380622081ed0bcc5326e61c0a65dbea3743a1
noear <noear@live.cn>
2023-05-30 16:55:50 +0800

    2.3.2

--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/AppxMapper2.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/AppxMapper2.java
@@ -7,6 +7,5 @@ import org.apache.ibatis.annotations.Mapper;
 /**
  * @author noear 2021/9/3 created
  */
-@Mapper
 public interface AppxMapper2 extends BaseMapper<AppxModel> {
 }
--- 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/AppxMapperPlus.java
+++ 4.Solon-Data/demo4031-mybatisplus/src/main/java/demo4031/dso/mapper/AppxMapperPlus.java
@@ -7,6 +7,5 @@ import org.apache.ibatis.annotations.Mapper;
 /**
  * @author noear 2022/3/28 created
  */
-@Mapper
 public interface AppxMapperPlus extends MyBaseMapper<AppxModel> {
 }
