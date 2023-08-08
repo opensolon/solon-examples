dd05e7455f19e0b7c93f58b9e900ee0ebbc46fe5
noear <noear@live.cn>
2023-08-08 08:33:33 +0800

    2.4.3-SNAPSHOT

---
+++ 9.Solon-Cloud/demo90a1-micrometer_prometheus/src/main/java/demo90a1/Config.java
@@ -0,0 +1,23 @@
+package demo90a1;
+
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Configuration;
+
+/**
+ * @author noear 2023/8/5 created
+ */
+@Configuration
+public class Config {
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
+    }
+}
