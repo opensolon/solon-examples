ee9cab55ab2c2cd9c16b9ef6daaf8eec74eb98fd
noear <noear@live.cn>
2023-11-28 01:17:04 +0800

    2.6.1

--- 8.Solon-Remoting-SocketD/demo8031-subscribe/src/main/java/demo8031/server/SessionUtil.java
+++ 8.Solon-Remoting-SocketD/demo8031-subscribe/src/main/java/demo8031/server/SessionUtil.java
@@ -9,7 +9,7 @@ import org.noear.socketd.transport.core.entity.StringEntity;
 public class SessionUtil {
     public static void broadcast(Message message) throws Exception {
         for (Session session : ServerListener.getOpenSessions()) {
-            session.send(message.route(), message.entity());
+            session.send(message.event(), message.entity());
         }
     }
 
