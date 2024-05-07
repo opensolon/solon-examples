17ac21361dcfb2643f0c11eba93739e62587dea7
ocean0551 <ocean.zhc@my.tccd.edu>
2024-05-07 18:09:22 +0800

    [feature] (demo4074-hasor_dataway-mds) Implementation of dataway multiple data source switching function

---
+++ 4.Solon-Data/demo4074-hasor_dataway-mds/src/main/java/webapp/dso/module/StartModule.java
@@ -0,0 +1,61 @@
+package webapp.dso.module;
+
+import net.hasor.dataql.DimUdf;
+import net.hasor.dataql.DimUdfSource;
+import net.hasor.dataql.Finder;
+import net.hasor.dataql.QueryApiBinder;
+import net.hasor.dataql.fx.db.FxSqlCheckChainSpi;
+import net.hasor.dataway.dal.providers.db.InformationStorage;
+import net.hasor.db.JdbcModule;
+import net.hasor.db.Level;
+import net.hasor.web.WebApiBinder;
+import net.hasor.web.WebModule;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.Component;
+import org.noear.solon.annotation.Inject;
+import webapp.dso.spi.FxSqlCheckChain;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Component
+public class StartModule implements WebModule {
+    @Inject("metadataDs")
+    DataSource metadataDs;
+    @Inject("dataDs1")
+    DataSource dataDs1;
+    @Inject("dataDs2")
+    DataSource dataDs2;
+
+
+    @Override
+    public void loadModule(WebApiBinder apiBinder) throws Throwable {
+        apiBinder.setEncodingCharacter("UTF-8", "UTF-8");
+        //
+        // .check dataSource
+        Objects.requireNonNull(this.metadataDs, "metadataDs is null");
+        Objects.requireNonNull(this.dataDs1, "dataDs1 is null");
+        Objects.requireNonNull(this.dataDs2, "dataDs2 is null");
+        //
+        // .isolation meta-tables using InformationStorage
+        apiBinder.bindType(InformationStorage.class).toInstance(() -> this.metadataDs);
+
+        //
+        // .add two data sources in to DataWay
+        apiBinder.installModule(new JdbcModule(Level.Full, this.metadataDs));
+        apiBinder.installModule(new JdbcModule(Level.Full, "ds1", this.dataDs1));
+        apiBinder.installModule(new JdbcModule(Level.Full, "ds2", this.dataDs2));
+
+        // 打印sql日志
+        apiBinder.bindSpiListener(FxSqlCheckChainSpi.class, FxSqlCheckChain.getInstance());
+        // 数据权限参数
+        //apiBinder.bindSpiListener(PreExecuteChainSpi.class, PreExecuteChain.getInstance());
+
+        //
+        // udf/udfSource/import 指令 的类型创建委托给 spring
+        QueryApiBinder queryBinder = apiBinder.tryCast(QueryApiBinder.class);
+        queryBinder.bindFinder(Finder.TYPE_SUPPLIER.apply(Solon.cfg()::getBean));
+        queryBinder.loadUdf(queryBinder.findClass(DimUdf.class));
+        queryBinder.loadUdfSource(queryBinder.findClass(DimUdfSource.class));
+    }
+}
