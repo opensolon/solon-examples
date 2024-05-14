21c87ae01cf5a3ffb85b54fb827630604b2c287d
noear <noear@live.cn>
2024-05-14 15:37:54 +0800

    添加 activemq 消息事务代码

--- 9.Solon-Cloud/demo9030-event_activemq/src/main/java/demo9030/producer/controller/TestController.java
+++ 9.Solon-Cloud/demo9030-event_activemq/src/main/java/demo9030/producer/controller/TestController.java
@@ -6,6 +6,9 @@ import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.cloud.CloudClient;
 import org.noear.solon.cloud.model.Event;
+import org.noear.solon.cloud.model.EventTran;
+import org.noear.solon.data.annotation.Tran;
+import org.noear.solon.data.tran.TranUtils;
 
 import java.util.Date;
 
@@ -46,5 +49,56 @@ public class TestController {
         HelloEntity entity = new HelloEntity();
         entity.name = "noear";
         return entity.publish();
+    }
+
+
+
+    @Mapping("/tran")
+    public Object tran() {
+        EventTran eventTran = CloudClient.event().newTran();
+
+        try {
+            CloudClient.event().publish(new Event("hello.demo", "test1").tran(eventTran));
+            CloudClient.event().publish(new Event("hello.demo", "test2").tran(eventTran));
+            CloudClient.event().publish(new Event("hello.demo", "test3").tran(eventTran));
+
+            eventTran.commit();
+            return true;
+        } catch (Throwable e) {
+            e.printStackTrace();
+
+            eventTran.rollback();
+            return false;
+        }
+    }
+
+    @Mapping("/tran2")
+    public Object tran2() {
+        EventTran eventTran = CloudClient.event().newTran();
+
+        try {
+            CloudClient.event().publish(new Event("hello.demo", "test1").tran(eventTran));
+            CloudClient.event().publish(new Event("hello.demo", "test2").tran(eventTran));
+            CloudClient.event().publish(new Event("hello.demo", "test3").tran(eventTran));
+            throw new IllegalStateException("");
+        } catch (Throwable e) {
+            e.printStackTrace();
+
+            eventTran.rollback();
+            return true;
+        }
+    }
+
+    @Tran
+    @Mapping("/tran3")
+    public Object tran3() {
+        EventTran eventTran = CloudClient.event().newTran();
+        TranUtils.listen(eventTran);
+
+        CloudClient.event().publish(new Event("hello.demo", "test1").tran(eventTran));
+        CloudClient.event().publish(new Event("hello.demo", "test2").tran(eventTran));
+        CloudClient.event().publish(new Event("hello.demo", "test3").tran(eventTran));
+
+        return true;
     }
 }
