ee67ba342b166fba26561a44ac90da155eba8b47
noear <noear@live.cn>
2024-10-09 22:54:58 +0800

    3.0.2-SNAPSHOT

---
+++ 4.Solon-Data/demo4010-sqlutils/src/test/java/features/Appx.java
@@ -0,0 +1,15 @@
+package features;
+
+/**
+ * @author noear 2024/10/9 created
+ */
+public class Appx {
+    public int app_id;
+
+    @Override
+    public String toString() {
+        return "Appx{" +
+                "app_id=" + app_id +
+                '}';
+    }
+}
