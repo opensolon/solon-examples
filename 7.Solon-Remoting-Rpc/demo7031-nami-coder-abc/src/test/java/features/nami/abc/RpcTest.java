a5ace985dc59f31a09830248c93d3a7637c312d7
noear <noear@live.cn>
2024-12-04 10:26:00 +0800

    3.0.4

--- 7.Solon-Remoting-Rpc/demo7031-nami-coder-abc/src/test/java/features/nami/abc/RpcTest.java
+++ 7.Solon-Remoting-Rpc/demo7031-nami-coder-abc/src/test/java/features/nami/abc/RpcTest.java
@@ -6,7 +6,11 @@ import demo7031.OrderDo;
 import org.junit.jupiter.api.Test;
 import org.noear.nami.annotation.NamiClient;
 import org.noear.nami.common.ContentTypes;
+import org.noear.solon.net.http.HttpUtils;
+import org.noear.solon.serialization.abc.AbcBytesSerializer;
 import org.noear.solon.test.SolonTest;
+
+import java.io.IOException;
 
 /**
  * @author noear 2024/12/3 created
@@ -17,11 +21,29 @@ public class RpcTest {
     HelloService helloService;
 
     @Test
-    public void hello() {
+    public void nami() {
         OrderDo orderDo = new OrderDo();
         orderDo.setOrderId(3);
 
         OrderDo orderDo2 = helloService.hello(orderDo);
+
+        System.out.println(orderDo2);
+
+        assert orderDo2 != null;
+        assert orderDo2.getOrderId() == orderDo.getOrderId();
+    }
+
+    @Test
+    public void http() throws IOException {
+        OrderDo orderDo = new OrderDo();
+        orderDo.setOrderId(3);
+
+        OrderDo orderDo2 = HttpUtils.http("http://localhost:8080/rpc/hello/hello")
+                .serializer(AbcBytesSerializer.getInstance())
+                .header(ContentTypes.HEADER_CONTENT_TYPE, ContentTypes.ABC_VALUE)
+                .header(ContentTypes.HEADER_SERIALIZATION, ContentTypes.AT_ABC)
+                .bodyOfBean(orderDo)
+                .postAs(OrderDo.class);
 
         System.out.println(orderDo2);
 
--- 7.Solon-Remoting-Rpc/demo7032-nami-coder-protostuff/src/test/java/features/nami/protostuff/RpcTest.java
+++ 7.Solon-Remoting-Rpc/demo7032-nami-coder-protostuff/src/test/java/features/nami/protostuff/RpcTest.java
@@ -6,7 +6,11 @@ import demo7032.OrderDo;
 import org.junit.jupiter.api.Test;
 import org.noear.nami.annotation.NamiClient;
 import org.noear.nami.common.ContentTypes;
+import org.noear.solon.net.http.HttpUtils;
+import org.noear.solon.serialization.protostuff.ProtostuffBytesSerializer;
 import org.noear.solon.test.SolonTest;
+
+import java.io.IOException;
 
 /**
  * @author noear 2024/12/3 created
@@ -17,11 +21,29 @@ public class RpcTest {
     HelloService helloService;
 
     @Test
-    public void hello() {
+    public void nami() {
         OrderDo orderDo = new OrderDo();
         orderDo.setOrderId(3);
 
         OrderDo orderDo2 = helloService.hello(orderDo);
+
+        System.out.println(orderDo2);
+
+        assert orderDo2 != null;
+        assert orderDo2.getOrderId() == orderDo.getOrderId();
+    }
+
+    @Test
+    public void http() throws IOException {
+        OrderDo orderDo = new OrderDo();
+        orderDo.setOrderId(3);
+
+        OrderDo orderDo2 = HttpUtils.http("http://localhost:8080/rpc/hello/hello")
+                .serializer(ProtostuffBytesSerializer.getInstance())
+                .header(ContentTypes.HEADER_CONTENT_TYPE, ContentTypes.PROTOBUF_VALUE)
+                .header(ContentTypes.HEADER_SERIALIZATION, ContentTypes.AT_PROTOBUF)
+                .bodyOfBean(orderDo)
+                .postAs(OrderDo.class);
 
         System.out.println(orderDo2);
 
