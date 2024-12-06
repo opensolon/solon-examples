157694ac173fbd626830448f0b532d3f93d847da
noear <noear@live.cn>
2024-12-06 11:12:16 +0800

    3.0.4

--- b.Integration/demoB001-kafka/src/main/java/demoB001/KafkaConfig.java
+++ b.Integration/demoB001-kafka/src/main/java/demoB001/KafkaConfig.java
@@ -1,23 +1,18 @@
 package demoB001;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
 
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Properties;
 
 /**
  * @author noear 2024/12/6 created
  */
 @Configuration
-public class Config {
+public class KafkaConfig {
     @Bean
     public KafkaProducer<String, String> producer(@Inject("${solon.kafka.properties}") Properties common,
                                              @Inject("${solon.kafka.producer}") Properties producer) {
