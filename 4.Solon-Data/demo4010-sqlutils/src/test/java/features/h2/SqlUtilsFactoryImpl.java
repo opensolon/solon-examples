1acaab6b8cbc494463aaaddb0a7ac32f40f6c22b
noear <noear@live.cn>
2024-12-23 09:07:40 +0800

    简化 SqlUtilsFactoryImpl

--- 4.Solon-Data/demo4010-sqlutils/src/test/java/features/h2/SqlUtilsFactoryImpl.java
+++ 4.Solon-Data/demo4010-sqlutils/src/test/java/features/h2/SqlUtilsFactoryImpl.java
@@ -2,9 +2,8 @@ package features.h2;
 
 import org.noear.solon.annotation.Component;
 import org.noear.solon.data.sql.SqlExecutor;
-import org.noear.solon.data.sql.SqlUtils;
 import org.noear.solon.data.sql.SqlUtilsFactory;
-import org.noear.solon.data.sql.impl.SimpleSqlUtils;
+import org.noear.solon.data.sql.impl.SimpleSqlExecutor;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -15,16 +14,11 @@ import java.util.Arrays;
 @Component
 public class SqlUtilsFactoryImpl implements SqlUtilsFactory {
     @Override
-    public SqlUtils create(DataSource ds) {
-        return new SimpleSqlUtils(ds){
-            @Override
-            public SqlExecutor sql(String sql, Object... args) {
-                System.out.println("sql:" + sql);
-                System.out.println("args:" + Arrays.toString(args));
-                System.out.println("----------");
+    public SqlExecutor create(DataSource ds, String sql, Object... args) {
+        System.out.println("sql:" + sql);
+        System.out.println("args:" + Arrays.toString(args));
+        System.out.println("----------");
 
-                return super.sql(sql, args);
-            }
-        };
+        return new SimpleSqlExecutor(ds, sql, args);
     }
 }
