8759896fb58beec841bae062ff630a55f32b760d
noear <noear@live.cn>
2025-01-08 09:11:07 +0800

    3.0.6-M3

--- 4.Solon-Data/demo4033-mybatisplus_sqlserver/src/main/java/demo4033/dso/mapper/AppxMapper.java
+++ 4.Solon-Data/demo4033-mybatisplus_sqlserver/src/main/java/demo4033/dso/mapper/AppxMapper.java
@@ -6,5 +6,5 @@ import demo4033.model.AppxModel;
 /**
  * @author noear 2021/9/3 created
  */
-public interface AppxMapper extends BaseMapper<demo4033.model.AppxModel> {
+public interface AppxMapper extends BaseMapper<AppxModel> {
 }
