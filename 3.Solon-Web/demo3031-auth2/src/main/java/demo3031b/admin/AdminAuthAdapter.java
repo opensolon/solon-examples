b5315cac168fa012c299f6208c0e05d97f92f75b
noear <noear@live.cn>
2025-02-21 23:09:10 +0800

    3.1.0-SNAPSHOT

--- 3.Solon-Web/demo3031-auth2/src/main/java/demo3031b/admin/AdminAuthAdapter.java
+++ 3.Solon-Web/demo3031-auth2/src/main/java/demo3031b/admin/AdminAuthAdapter.java
@@ -1,18 +1,18 @@
 package demo3031b.admin;
 
-import org.noear.solon.annotation.Component;
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Configuration;
 import org.noear.solon.auth.AuthAdapter;
-import org.noear.solon.auth.AuthAdapterSupplier;
 
 /**
  * @author noear 2022/8/15 created
  */
-@Component
-public class AdminAuthAdapterSupplierImpl implements AuthAdapterSupplier {
-    final AuthAdapter adminAuth;
-
-    public AdminAuthAdapterSupplierImpl(){
-        adminAuth = new AuthAdapter()
+@Configuration
+public class AdminAuthAdapter {
+    @Bean
+    public AuthAdapter initAuthAdapter() {
+        return new AuthAdapter()
+                .pathPrefix("/admin/")
                 .loginUrl("/admin/login") //设定登录地址，未登录时自动跳转
                 .addRule(r -> r.include("/admin/**").verifyIp().failure((c, t) -> c.output("你的IP不在白名单"))) //添加规则
                 .addRule(b -> b.include("/admin/**").exclude("/admin/login**").verifyPath()) //添加规则
@@ -20,14 +20,5 @@ public class AdminAuthAdapterSupplierImpl implements AuthAdapterSupplier {
                 .failure((ctx, rst) -> { //设定默认的验证失败处理
                     ctx.render(rst);
                 });
-    }
-
-    @Override
-    public String pathPrefix() {
-        return "/admin/";
-    }
-
-    public AuthAdapter adapter(){
-        return adminAuth;
     }
 }
