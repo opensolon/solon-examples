0347eb64742c4782f07b2be90600b67263984954
noear <noear@live.cn>
2025-04-15 20:31:06 +0800

    3.1.3-RC

--- c.Solon-AI/demoC001-ai/src/main/java/demoC001/controller/FunctionController.java
+++ c.Solon-AI/demoC001-ai/src/main/java/demoC001/controller/FunctionController.java
@@ -1,8 +1,8 @@
 package demoC001.controller;
 
 import org.noear.solon.ai.chat.ChatModel;
-import org.noear.solon.ai.chat.annotation.FunctionMapping;
-import org.noear.solon.ai.chat.annotation.FunctionParam;
+import org.noear.solon.ai.chat.annotation.ToolMapping;
+import org.noear.solon.ai.chat.annotation.ToolParam;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.annotation.Mapping;
@@ -23,7 +23,7 @@ public class FunctionController {
     @Mapping("weather")
     public String weather(@Param(defaultValue = "今天杭州的天气如何？") String message) throws IOException {
         return chatModel.prompt(message)
-                .options(o -> o.functionAdd(Tools.instance))
+                .options(o -> o.toolsAdd(Tools.instance))
                 .call().getMessage().getContent();
     }
 
@@ -31,8 +31,8 @@ public class FunctionController {
         static final Tools instance = new Tools();
 
         //天气查询
-        @FunctionMapping(description = "获取指定城市的天气情况")
-        public String get_weather(@FunctionParam(description = "根据用户提到的地点推测城市") String location) {
+        @ToolMapping(description = "获取指定城市的天气情况")
+        public String get_weather(@ToolParam(description = "根据用户提到的地点推测城市") String location) {
             if (location == null) {
                 throw new IllegalStateException("arguments location is null (Assistant recognition failure)");
             }
