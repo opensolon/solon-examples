f8e80fb95a071e54861274e6729124c31839f9e2
noear <noear@live.cn>
2025-04-18 21:29:09 +0800

    3.2.0

---
+++ 1.Solon/demo1022-log_log4j2/src/test/java/demo/LogTest.java
@@ -0,0 +1,16 @@
+package demo;
+
+import demo1022.App;
+import org.junit.jupiter.api.Test;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2025/4/18 created
+ */
+@SolonTest(App.class)
+public class LogTest {
+    @Test
+    public void case1() {
+
+    }
+}
