f6da567e121defd64db1c31c259a3f5d93ee27d7
noear <noear@live.cn>
2025-12-04 18:22:51 +0800

    3.7.3

--- 7.Solon-Remoting-Rpc/demo7021-feign/src/main/java/demo7021/dso/api/RemoteService.java
+++ 7.Solon-Remoting-Rpc/demo7021-feign/src/main/java/demo7021/dso/api/RemoteService.java
@@ -1,5 +1,7 @@
 package demo7021.dso.api;
 
+import feign.Body;
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 import demo7021.model.User;
@@ -7,8 +9,13 @@ import demo7021.model.User;
 public interface RemoteService {
 
     @RequestLine("GET /users/get1?name={name}")
-    String getOwner(@Param(value = "name") String name);
+    String getOwner(@Param("name") String name);
 
     @RequestLine("GET /users/get2?name={name}")
     User get2(String name);
+
+    @RequestLine("POST setJson")
+    @Headers("Content-Type: application/json")
+    @Body("{body}")
+    User setJson(@Param("body") String body);
 }
