9edf8c107a95cb15dc449edee573bc82d21b8071
noear <noear@live.cn>
2026-01-30 19:31:14 +0800

    3.9.0

--- 4.Solon-Data/demo4101-redisson/src/main/java/demo4101/Config.java
+++ 4.Solon-Data/demo4101-redisson/src/main/java/demo4101/Config.java
@@ -4,12 +4,13 @@ import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
 import org.redisson.api.RedissonClient;
+import org.redisson.solon.RedissonClientOriginalSupplier;
 import org.redisson.solon.RedissonSupplier;
 
 @Configuration
 public class Config {
     @Bean
-    public RedissonClient redis(@Inject("${test.redis1}")RedissonSupplier supplier){
+    public RedissonClient redis(@Inject("${test.redis1}") RedissonClientOriginalSupplier supplier){
         System.out.println("config - redis");
         return supplier.get();
     }
